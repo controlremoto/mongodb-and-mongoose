2cabe6b8c2edd9654da54414226c8db4317bc200
Monuel Heza <miguel3lp@hotmail.com>
2022-06-15 20:01:56 -0400

    feat:Create a Model
    #2 Create a Model, creates a model, Schema, and an instance of this model

--- myApp.js
+++ myApp.js
@@ -1,4 +1,24 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-console.log("The process.env.MONGO_URI value:",process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI);
+
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+const mySelf = new Person({
+  name: "Miguel",
+  age: 28,
+  favoriteFoods: ["Pizza", "Pasta"]
+})
+
+exports.PersonModel = Person;
